d409b53ae69c7c2f06eeb90787ddfd9e6c0d8b39
asadulR <asad1252001@gmail.com.com>
2022-04-27 14:46:41 +0600

    all job added to the ui and added spniine

--- src/Components/Job/Job/Job.js
+++ src/Components/Job/Job/Job.js
@@ -2,37 +2,45 @@ import React, { useEffect, useState } from 'react';
 import SingleJob from './SingleJob';
 import './Job.css';
 import { Link } from 'react-router-dom';
-import svg from '../../../Assets/images/wave (1).svg'
+import Loading from '../../Shared/Loading/Loading';
 const Job = () => {
     const [jobs, setJob] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
+        setLoading(true);
         fetch('http://localhost:4000/job')
             .then((response) => response.json())
-            .then((json) => setJob(json));
+            .then((json) => {
+                setJob(json)
+                setLoading(false);
+            });
     }, []);
 
 
     return (
-        <section>
-            <div className='relative'>
-                <div className='jobs-posts'>
-                    <img className='svg-img' src={svg} alt="" />
-                </div>
-                <div className='jobs-page'>
-                    <h2 className='text-center text-white fw-bold'>Job Posts</h2>
-                    <p className='text-white mt-3'><Link className='jobpage-link' to='/'>Home</Link>/<Link className='jobpage-link' to='/'>Job Pages</Link>/<Link className='jobpage-link' to='/job'>Jobs</Link></p>
-                </div>
-                <div className='jobs'>
-                    {
-                        jobs.map(job => <SingleJob
-                            key={job._id}
-                            job={job}
-                        >
-
-                        </SingleJob>)
-                    }
+        <section className='job-section'>
+            <div className='py-5'>
+                <div className='jobs-page py-2'>
+                    <h2 className='text-center text-success fw-bold'>Job Posts</h2>
+                    <p className='text-primary text-center mt-3'><Link className='jobpage-link' to='/'>Home</Link>/<Link className='jobpage-link' to='/'>Job Pages</Link>/<Link className='jobpage-link' to='/job'>Jobs</Link></p>
                 </div>
             </div>
+            <>
+                {
+                    loading ? (<Loading></Loading>) :
+                        (<div className='jobs container'>
+                            <h3 className='d-flex text-success align-items-center'>{jobs.length} <span className='ms-2'>job offers</span></h3>
+                            {
+                                jobs.map(job => <SingleJob
+                                    key={job._id}
+                                    job={job}
+                                >
+
+                                </SingleJob>)
+                            }
+                        </div>)
+                }
+            </>
         </section>
     );
 };
--- src/Components/Job/Job/SingleJob.js
+++ src/Components/Job/Job/SingleJob.js
@@ -1,9 +1,19 @@
+import { LocationMarkerIcon } from '@heroicons/react/solid';
 import React from 'react';
-
-const SingleJob = () => {
+import { Link } from 'react-router-dom';
+import './Job.css'
+const SingleJob = ({job}) => {
+    const {name,date, description, area, time, type} = job;
+    console.log(job)
     return (
-        <div>
-            
+        <div className='single-job p-4'>
+            <h5 className='d-flex align-items-center justify-content-between'><span>{name}</span> <small className='fs-6 gray'>{date}</small></h5>
+            <p className='d-flex align-items-center'><span className='small me-3'><LocationMarkerIcon className='location-icon'></LocationMarkerIcon>{area}</span> <span className='small me-2'>{type}</span> | <span className='small ms-2'>{time}</span></p>
+
+            <p className='gray'>{description}</p>
+            <div className='w-100 text-end'>
+                <Link className='fw-bold btn btn-outline-success' to='/'>Apply now</Link>
+            </div>
         </div>
     );
 };
